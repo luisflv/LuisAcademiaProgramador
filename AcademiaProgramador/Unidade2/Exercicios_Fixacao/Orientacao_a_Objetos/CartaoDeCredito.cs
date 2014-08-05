c0be6c7e9aff8a2f1ca7aee10c933aeed0a41694
luisflv <luis_flv@hotmail.com.br>
2014-08-04 23:09:26 -0300

    Atualizacao Repositorio

---
+++ AcademiaProgramador/Unidade2/Exercicios_Fixacao/Orientacao_a_Objetos/CartaoDeCredito.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade2.Exercicios_Fixacao.Orientacao_a_Objetos
+{
+    class CartaoDeCredito
+    {
+        public int numero;
+        public string dataValidade;
+    }
+}
