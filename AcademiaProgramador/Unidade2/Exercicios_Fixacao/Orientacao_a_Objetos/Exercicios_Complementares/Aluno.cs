05406f0af9b6affe3ec7f8c53d7c94fd07948636
luisflv <luis_flv@hotmail.com.br>
2014-08-04 23:36:34 -0300

    Atualizacao Repositorio

---
+++ AcademiaProgramador/Unidade2/Exercicios_Fixacao/Orientacao_a_Objetos/Exercicios_Complementares/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade2.Exercicios_Fixacao.Orientacao_a_Objetos
+{
+    class Aluno
+    {
+        public string nome;
+        public string rg;
+        public DateTime dataNasc;
+    }
+}
---
+++ AcademiaProgramador/Unidade2/Exercicios_Fixacao/Orientacao_a_Objetos/Exercicios_Fixacao/Agencia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade2.Exercicios_Fixacao.Orientacao_a_Objetos
+{
+    class Agencia
+    {
+        public int numero;
+    }
+}
